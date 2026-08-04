e0bc20c568e273167f4e582d7e6b6414940c7797
ChristiamGeredaE <christiamalexandergeredaestrad@gmail.com>
2026-08-03 21:04:21 -0600

    sistema-control-fletes

---
+++ control-fletes/src/main/java/com/sistema/control_fletes/ControlFletesApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.control_fletes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlFletesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlFletesApplication.class, args);
+	}
+
+}
---
+++ control-fletes/src/test/java/com/sistema/control_fletes/ControlFletesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.control_fletes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlFletesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
