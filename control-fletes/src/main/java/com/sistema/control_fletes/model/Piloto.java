144bd85a7d47504c1fb7cf7169ec7b385758f263
ChristiamGeredaE <christiamalexandergeredaestrad@gmail.com>
2026-08-06 00:30:18 -0600

    Agregando cambios a la entidad Piloto

--- control-fletes/src/main/java/com/sistema/control_fletes/model/Piloto.java
+++ control-fletes/src/main/java/com/sistema/control_fletes/model/Piloto.java
@@ -1,5 +1,30 @@
 package com.sistema.control_fletes.model;
 
-public class Piloto {
+import java.lang.annotation.Inherited;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+@Table(name ="pilotos")
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Piloto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre",nullable = false,length = 80)
+    private String nombre;
+
+    @Column(name ="apellido",nullable = false,length = 80)
+    private String apellido;
+
+    @Column(name = "doc_identidad",nullable = false,length = 15)
+    private String dpi;
+
+    @OneToMany(mappedBy = "piloto" ,fetch = FetchType.LAZY)
+    @JsonManagedReference("piloto-viajes")
+    private List<Viaje> viajes = new ArrayList<>();
 }
